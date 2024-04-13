b8ca025cdbd2f4d1798346eefc49870d923bc518
dhivyap0403 <dhivyap0403@gmail.com>
2024-04-14 00:18:49 +0530

    create signup api

---
+++ api/controllers/authController.js
@@ -0,0 +1,16 @@
+// authController.js
+
+import User from "../models/userModel.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ name, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js';
+import authRoutes from './routes/authRoute.js';
 
 
 dotenv.config();
+
 
 
 mongoose.connect(process.env.MONGO).then(()=>
@@ -15,6 +17,7 @@ mongoose.connect(process.env.MONGO).then(()=>
     console.log(err);
 });
 const app=express();
+app.use(express.json());
 
 app.listen(3000,()=>{
 
@@ -22,3 +25,4 @@ app.listen(3000,()=>{
 
 }); 
 app.use("/api/user",userRoutes);
+app.use("/api/auth",authRoutes)
