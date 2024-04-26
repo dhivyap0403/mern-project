4c1c61d7d03f352993ddcd47706d87cf00734509
dhivyap0403 <dhivyap0403@gmail.com>
2024-04-26 22:24:46 +0530

    update  header and make profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import {Route,Routes,BrowserRouter} from'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Profile from './pages/Profile';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import Header from './component/Header';
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Header from "./component/Header";
+import PrivateRoute from "./component/PrivateRoute";
 
 export default function App() {
-  return (<BrowserRouter>
-  <Header/>
-  <Routes>
-  <Route  path="/" element={<Home/>}/>
-  <Route  path="/about" element={<About/>}/>
-  <Route  path="/signin" element={<Signin/>}/>
-  <Route  path="/signup" element={<Signup/>}/>
-  <Route  path="/profile" element={<Profile/>}/>
-    </Routes>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<PrivateRoute />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,27 +1,34 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-    return (
-        <div className='bg-slate-200'>
-          <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <Link to='/'>
-              <h1 className='font-bold'>Auth App</h1>
-            </Link>
-            <ul className='flex gap-4'>
-              <Link to='/'>
-                <li>Home</li>
-              </Link>
-              <Link to='/about'>
-                <li>About</li>
-              </Link>
-              <Link to='/signin'>
-                
-                  <li>Sign In</li>
-        
-              </Link>
-            </ul>
-          </div>
-        </div>
-      );
-    }
-    
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="bg-slate-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold">Auth App</h1>
+        </Link>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avator}
+                alt="profile"
+              />
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign In</li>
+            )}
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+}
